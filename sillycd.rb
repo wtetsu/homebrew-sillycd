e5bccb9ba0f8fc24700d23cecb7fd04eef2de9a8
wtetsu <tewatana@gmail.com>
2020-01-08 22:02:25 +0900

    Formula

---
+++ sillycd.rb
@@ -0,0 +1,18 @@
+class Sillycd < Formula
+  desc "The fastest way to type cd command"
+  homepage "https://github.com/wtetsu/sillycd/"
+  url "https://github.com/wtetsu/sillycd.git",
+      :tag      => "v1.0.0",
+      :revision => "9294377a976b43d7b8ae0c0b0010378f693b42a1"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-s -w", "-trimpath", "-o", bin/"sillycd"
+    prefix.install_metafiles
+  end
+
+  test do
+    system "true"
+  end
+end
