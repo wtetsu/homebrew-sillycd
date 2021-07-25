4c6caecd16bb666e80d4ac791729bbe7fcc638c3
wtetsu <tewatana@gmail.com>
2021-07-25 13:59:29 +0900

    Minor polishing

--- sillycd.rb
+++ sillycd.rb
@@ -1,9 +1,9 @@
 class Sillycd < Formula
-  desc "The fastest way to type cd command"
+  desc "Fastest way to type cd command"
   homepage "https://github.com/wtetsu/sillycd/"
   url "https://github.com/wtetsu/sillycd.git",
-      :tag      => "v1.0.0",
-      :revision => "9294377a976b43d7b8ae0c0b0010378f693b42a1"
+      tag:      "v1.0.0",
+      revision: "9294377a976b43d7b8ae0c0b0010378f693b42a1"
 
   depends_on "go" => :build
 
